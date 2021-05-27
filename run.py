b5b296b49481f79ad425b715505be6e387c0aacf
Eric <ericyeonpark@gmail.com>
2021-05-27 10:13:27 -0700

    removed insights and process from nav bar. edited Erics name

--- run.py
+++ run.py
@@ -15,8 +15,8 @@ navbar = dbc.NavbarSimple(
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
-        dbc.NavItem(dcc.Link('Insights', href='/insights', className='nav-link')), 
-        dbc.NavItem(dcc.Link('Process', href='/process', className='nav-link')), 
+        # dbc.NavItem(dcc.Link('Insights', href='/insights', className='nav-link')), 
+        # dbc.NavItem(dcc.Link('Process', href='/process', className='nav-link')), 
     ],
     sticky='top',
     color='light', 
@@ -35,7 +35,7 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span(['Erick Park, ', 'Sal Hossain, ', 'Mena Ekelemu, ', 'Samantha Temple'], className='mr-2'), 
+                    html.Span(['Eric Park, ', 'Sal Hossain, ', 'Mena Ekelemu, ', 'Samantha Temple'], className='mr-2'), 
                     html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
                     html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
                     html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
